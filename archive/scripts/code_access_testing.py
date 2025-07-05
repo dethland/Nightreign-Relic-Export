a16efe051acc451e8b068c705980b048eae1ae57
dethland <jasonli1919176@gmail.com>
2025-07-05 20:02:14 +0800

    Update .gitignore to exclude archive folder

--- archive/scripts/code_access_testing.py
+++
@@ -1,7 +0,0 @@
-import focused_window
-
-
-
-if __name__ == "__main__":
-    a = focused_window.get_active_window_app_name()
-    print(a)
--- archive/scripts/focused_window.py
+++
@@ -1,5 +0,0 @@
-import win32gui
-
-def get_active_window_app_name():
-    hwnd = win32gui.GetForegroundWindow()
-    return win32gui.GetWindowText(hwnd)
